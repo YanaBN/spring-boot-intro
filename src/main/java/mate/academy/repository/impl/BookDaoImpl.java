7a565b94fb0b4b0db7c3c78e7193c7fbf8ed97dc
Yana Bilyk <bilyk.yn@gmail.com>
2025-08-13 14:43:47 +0200

    Changed mapping to "/api/books"

--- src/main/java/mate/academy/repository/impl/BookDaoImpl.java
+++ src/main/java/mate/academy/repository/impl/BookDaoImpl.java
@@ -1,59 +1,57 @@
 package mate.academy.repository.impl;
 
 import java.util.List;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import mate.academy.exeptions.DataProcessingException;
 import mate.academy.model.Book;
 import mate.academy.repository.BookRepository;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class BookDaoImpl implements BookRepository {
 
-    private final SessionFactory sessionFactory;
+    private final EntityManagerFactory entityManagerFactory;
 
     @Override
     public Book save(Book book) {
-        Session session = null;
-        Transaction transaction = null;
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = null;
         try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            session.save(book);
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(book);
             transaction.commit();
             return book;
         } catch (Exception e) {
-            if (transaction != null) {
+            if (transaction != null && transaction.isActive()) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't save book into DB: " + book, e);
+            throw new DataProcessingException("Can't save book into DB:" + book, e);
         } finally {
-            if (session != null) {
-                session.close();
-            }
+            entityManager.close();
         }
-
     }
 
     @Override
     public List<Book> findAll() {
-        try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("SELECT u FROM Book u", Book.class).getResultList();
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            return entityManager.createQuery("SELECT b FROM Book b", Book.class).getResultList();
         } catch (Exception e) {
-            throw new DataProcessingException("Can't find any books ", e);
+            throw new DataProcessingException("Can't find any books in DB", e);
         }
     }
 
     @Override
     public Optional<Book> findById(Long id) {
-        try (Session session = sessionFactory.openSession()) {
-            Book book = session.find(Book.class, id);
-            return Optional.ofNullable(book);
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            return Optional.ofNullable(entityManager.find(Book.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Error while finding book by id " + id, e);
         }
     }
 }
