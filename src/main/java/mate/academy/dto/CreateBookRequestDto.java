81e25affa8e4b8da748c06b38acefb1424c77a5a
Yana Bilyk <bilyk.yn@gmail.com>
2025-08-18 14:23:33 +0200

    ran checkstyle:check

--- src/main/java/mate/academy/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/dto/CreateBookRequestDto.java
@@ -1,9 +1,8 @@
 package mate.academy.dto;
 
-import java.math.BigDecimal;
-
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 
