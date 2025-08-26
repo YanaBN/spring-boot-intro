87733aa39a4b3dc01b88f3c824dc2b4e18fcfa6a
Yana Bilyk <bilyk.yn@gmail.com>
2025-08-26 19:19:22 +0200

    work on category service

--- src/main/java/mate/academy/service/impl/CategoryServiceImpl.java
+++ src/main/java/mate/academy/service/impl/CategoryServiceImpl.java
@@ -1,30 +1,47 @@
 package mate.academy.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import mate.academy.dto.CategoryDto;
+import mate.academy.exeptions.EntityNotFoundException;
+import mate.academy.mapper.CategoryMapper;
+import mate.academy.model.Category;
+import mate.academy.repository.CategoryRepository;
 import mate.academy.service.CategoryService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
+   private final CategoryRepository categoryRepository;
+   private final CategoryMapper categoryMapper;
+
     @Override
     public List findAll() {
-        return null;
+        return categoryRepository.findAll();
     }
 
     @Override
     public CategoryDto getById(Long id) {
-        return null;
+        Category category = categoryRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find category by id: " + id)
+        );
+        return categoryMapper.toDto(category);
     }
 
     @Override
     public CategoryDto save(CategoryDto categoryDto) {
-        return null;
+        Category category = categoryMapper.toEntity(categoryDto);
+        return categoryMapper.toDto(categoryRepository.save(category));
     }
 
     @Override
     public CategoryDto update(Long id, CategoryDto categoryDto) {
+        Category category = categoryRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't update category by id: " + id)
+        );
+        categoryMapper.
         return null;
     }
 
